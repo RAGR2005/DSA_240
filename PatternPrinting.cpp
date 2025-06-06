a96340d4e3077a470b0a61efcc147894be8576ca
Raghav Garg <111raghavgarg@gmail.com>
2025-06-06 18:24:23 +0530

    Update PatternPrinting.cpp

--- PatternPrinting.cpp
+++ PatternPrinting.cpp
@@ -742,3 +742,343 @@ int main()
 }
 
 */
+
+/*
+
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+
+
+void print3(int n){
+    int start=1;
+    for(int i=0;i<n;i++){
+            if(i%2==0){
+                start=1;
+            }
+            else {
+                start=0;
+            }
+            for(int j=0;j<=i;j++){
+                cout<<start;
+                start=1-start;
+            }
+               cout<<endl;
+        }
+     
+}
+        
+int main()
+{
+    int n;
+    cin >> n;
+    // print1(n);
+    // print2(n);
+    print3(n);
+
+}
+*/
+/*
+1             1
+1 2         2 1
+1 2 3     3 2 1
+1 2 3 4 4 3 2 1
+
+void print3(int n){
+    for(int i=1;i<=n;i++){
+            //numbers
+            for(int j=1;j<=i;j++){
+                cout<<j;
+            }
+
+            //space
+            for(int j=1;j<=(2*n)-(2*i);j++){
+                cout<<" ";
+            }
+
+            //numbers
+            for(int j=i;j>=1;j--){
+                cout<<j;
+            }
+               cout<<endl;
+        }
+     
+}
+        
+int main()
+{
+    int n;
+    cin >> n;
+    // print1(n);
+    // print2(n);
+    print3(n);
+
+}
+*/
+
+/*
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+
+void print3(int n){
+    int num=1;
+    for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<num<<" ";
+                num=num+1;
+            }
+               cout<<endl;
+        }
+     
+}
+        
+int main()
+{
+    int n;
+    cin >> n;
+    // print1(n);
+    // print2(n);
+    print3(n);
+
+}
+*/
+
+
+/*
+A
+A B
+A B C
+A B C D
+A B C D E
+
+void print3(int n){
+    
+    for(int i=1;i<=n;i++){
+        char num='A';
+            for(int j=1;j<=i;j++){
+                cout<<num<<" ";
+                num=num+1;
+            }
+               cout<<endl;
+        }
+     
+}
+OTHER APPROACH
+Void print3(int n){
+    
+    for(int i=0;i<n;i++){
+            for(char j='A';j<='A'+i;j++){
+                cout<<j<<" ";
+            }
+               cout<<endl; 
+        }
+     
+        
+int main()
+{
+    int n;
+    cin >> n;
+    // print1(n);
+    // print2(n);
+    print3(n);
+
+}
+
+*/
+
+/*
+A
+B B 
+C C C
+D D D D 
+E E E E E
+
+
+void print3(int n){
+     char num='A';
+    for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<num<<" ";
+            }
+            num=num+1;
+               cout<<endl;
+        }
+     
+}
+        
+int main()
+{
+    int n;
+    cin >> n;
+    // print1(n);
+    // print2(n);
+    print3(n);
+
+}
+*/
+
+/*
+      A
+    A B A
+  A B C B A
+A B C D C B A
+
+
+
+void print3(int n){
+   
+    for(int i=0;i<n;i++){
+            //SPACE
+            for(int j=0;j<n-1-i;j++){
+                cout<<" ";
+            }
+            //Chars
+            for(char j='A';j<='A'+i;j++){
+                    cout<<j;
+            }
+
+            for(char j='A'+i-1;j>='A';j--){
+                cout<<j;
+            }
+            cout<<endl;
+        }
+     
+}
+        
+int main()
+{
+    int n;
+    cin >> n;
+    // print1(n);
+    // print2(n);
+    print3(n);
+}
+
+*/
+
+
+/*
+E
+DE
+CDE
+BCDE
+ABCDE
+
+
+void print3(int n){
+    for(int i=1;i<=n;i++){
+        for(char j='A'+n-i;j<='A'+n-1;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+     
+}
+        
+int main()
+{
+    int n;
+    cin >> n;
+    // print1(n);
+    // print2(n);
+    print3(n);
+}
+*/
+
+
+/*
+* * * *
+*     *
+*     *
+* * * *
+
+
+
+
+void print3(int n){
+    for(int i=1;i<=n;i++){
+
+        for(int j=1;j<=n;j++){
+            if(i==1 || i==n || j==1 || j==n){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        
+        cout<<endl;
+    }
+     
+}
+        
+int main()
+{
+    int n;
+    cin >> n;
+    // print1(n);
+    // print2(n);
+    print3(n);
+}
+
+*/
+
+
+/*
+6 6 6 6 6 6 6 6 6 6 6 
+6 5 5 5 5 5 5 5 5 5 6 
+6 5 4 4 4 4 4 4 4 5 6 
+6 5 4 3 3 3 3 3 4 5 6 
+6 5 4 3 2 2 2 3 4 5 6 
+6 5 4 3 2 1 2 3 4 5 6 
+6 5 4 3 2 2 2 3 4 5 6 
+6 5 4 3 3 3 3 3 4 5 6 
+6 5 4 4 4 4 4 4 4 5 6 
+6 5 5 5 5 5 5 5 5 5 6 
+6 6 6 6 6 6 6 6 6 6 6
+
+
+void print3(int n)
+{
+     // Outer loop for no. of rows
+     for(int i=0;i<2*n-1;i++){
+         
+         // inner loop for no. of columns.
+         for(int j=0;j<2*n-1;j++){
+             
+             // Initialising the top, down, left and right indices of a cell.
+             int top = i;
+             int bottom = j;
+             int right = (2*n - 2) - j;
+             int left = (2*n - 2) - i;
+             
+             // Min of 4 directions and then we subtract from n
+             // because previously we would get a pattern whose border
+             // has 0's, but we want with border N's and then decreasing inside.
+             cout<<(n- min(min(top,bottom), min(left,right)))<<" ";
+         }
+         
+         // As soon as the numbers for each iteration are printed, we move to the
+         // next row and give a line break otherwise all numbers
+         // would get printed in 1 line.
+         cout<<endl;
+     }
+      
+}
+
+int main()
+{   
+    // Here, we have taken the value of N as 5.
+    // We can also take input from the user.
+    int N = 5;
+    
+    print3(N);
+
+    return 0;
+}
+*/
