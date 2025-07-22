d324aef4bbfa6cb85087ba850c686d9551515389
Raghav Garg <111raghavgarg@gmail.com>
2025-07-22 18:25:43 +0530

    Adding ques

---
+++ Leetcode_ques/Single_Number.cpp
@@ -0,0 +1,46 @@
+/*
+136. Single Number
+Solved
+Easy
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+Input: nums = [1]
+Output: 1
+
+Time Complexity: O(N*logM) + O(M), where M = size of the map i.e. M = (N/2)+1. N = size of the array.
+Reason: We are inserting N elements in the map data structure and insertion takes logM time(where M = size of the map). So this results in the first term O(N*logM). The second term is to iterate the map and search the single element. In Java, HashMap generally takes O(1) time complexity for insertion and search. In that case, the time complexity will be O(N) + O(M).
+
+Note: The time complexity will be changed depending on which map data structure we are using. If we use unordered_map in C++, the time complexity will be O(N) for the best and average case instead of O(N*logM). But in the worst case(which rarely happens), it will be nearly O(N2).
+
+Space Complexity: O(M) as we are using a map data structure. Here M = size of the map i.e. M = (N/2)+1.
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& arr) {
+        int n=arr.size();
+        map<int,int> mpp;
+        for(int i=0;i<n;i++){
+            mpp[arr[i]]++;
+        }
+
+        for(auto it:mpp){
+            if(it.second==1){
+            return it.first;
+            }
+        }
+        return -1;
+    }
+};
+*/
